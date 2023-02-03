791ee542e9894b4781488ef30c1a42bb4004f13f
Jmendo12 <jmendo.1024@gmail.com>
2023-02-02 16:42:54 -0800

    Add search & deletion to trie

--- src/Trie/Trie.spec.ts
+++ src/Trie/Trie.spec.ts
@@ -5,7 +5,7 @@ describe("TrieNode", () => {
     const node = new TrieNode();
 
     expect(node).not.toBeNull();
-    expect(node.children).toEqual([]);
+    expect(node.children).toEqual(new Array(26).fill(null));
     expect(node.wordCount).toEqual(0);
   });
 });
@@ -20,13 +20,66 @@ describe("Trie", () => {
     const trie = new Trie();
     trie.insert("and");
     // Check that each character was placed in the trie.
-    expect(trie.root.children[0]).not.toBeUndefined();
-    expect(trie.root.children[0].children[13]).not.toBeUndefined();
-    expect(trie.root.children[0].children[13].children[3]).not.toBeUndefined();
+    expect(trie.root.children[0]).not.toBeNull();
+    expect(trie.root.children[0]?.children[13]).not.toBeNull();
+    expect(trie.root.children[0]?.children[13]?.children[3]).not.toBeNull();
 
     trie.insert("ant");
-    expect(trie.root.children[0]).not.toBeUndefined();
-    expect(trie.root.children[0].children[13]).not.toBeUndefined();
-    expect(trie.root.children[0].children[13].children[19]).not.toBeUndefined();
+    expect(trie.root.children[0]).not.toBeNull();
+    expect(trie.root.children[0]?.children[13]).not.toBeNull();
+    expect(trie.root.children[0]?.children[13]?.children[19]).not.toBeNull();
+  });
+
+  it("can check if a prefix exists within the trie", () => {
+    const trie = new Trie();
+    trie.insert("and");
+    trie.insert("test");
+
+    expect(trie.hasPrefix("an")).toEqual(true);
+    expect(trie.hasPrefix("tes")).toEqual(true);
+    expect(trie.hasPrefix("false")).toEqual(false);
+  });
+
+  it("can check if a full word exists within the trie", () => {
+    const trie = new Trie();
+    trie.insert("and");
+    trie.insert("test");
+
+    expect(trie.hasWord("an")).toEqual(false);
+    expect(trie.hasWord("and")).toEqual(true);
+
+    expect(trie.hasWord("tes")).toEqual(false);
+    expect(trie.hasWord("test")).toEqual(true);
+
+    expect(trie.hasWord("false")).toEqual(false);
+  });
+
+  it("can delete a word from the trie", () => {
+    const trie = new Trie();
+    trie.insert("an");
+    trie.insert("and");
+    trie.insert("ant");
+
+    // Test the case where we delete a word that is the prefix of another word.
+    expect(trie.root.children[0]?.children[13]?.wordCount).toEqual(1);
+    expect(trie.delete("an")).toEqual(true);
+    expect(trie.root.children[0]?.children[13]?.wordCount).toEqual(0);
+
+    // Test the case where the word shares a common prefix with other words in the trie.
+    expect(trie.root.children[0]?.children[13]?.children[3]?.wordCount).toEqual(
+      1
+    );
+    expect(trie.delete("and")).toEqual(true);
+    expect(trie.root.children[0]?.children[13]?.children[3]).toBeNull();
+
+    // Test the case where the word does not share a common prefix with other words in the trie.
+    expect(
+      trie.root.children[0]?.children[13]?.children[19]?.wordCount
+    ).toEqual(1);
+    expect(trie.delete("ant")).toEqual(true);
+    expect(trie.root.children[0]).toBeNull();
+
+    // Test the case where the word does not exist in the trie.
+    expect(trie.delete("test")).toEqual(false);
   });
 });
--- src/Trie/Trie.ts
+++ src/Trie/Trie.ts
@@ -1,10 +1,10 @@
 export class TrieNode {
-  children: TrieNode[];
+  children: Array<TrieNode | null>;
   wordCount: number;
 
   constructor() {
     // An array of pointers representing each lower case letter.
-    this.children = new Array(26);
+    this.children = new Array(26).fill(null);
     this.wordCount = 0;
   }
 }
@@ -24,15 +24,100 @@ export class Trie {
       // because this will get us the actual numeric representation of the
       // character. Other methods will get us NaN. We subtract by 10 because
       // we want 'a' to be 0.
-      const characterIndex = parseInt(word.charAt(i), 36) - 10;
-      if (current.children[characterIndex] === undefined) {
+      const characterIndex = Trie.#getCharacterIndex(word, i);
+      if (!current.children[characterIndex]) {
         const newNode = new TrieNode();
         current.children[characterIndex] = newNode;
       }
 
-      current = current.children[characterIndex];
+      current = current.children[characterIndex]!;
     }
 
     current.wordCount++;
+  }
+
+  hasPrefix(word: string) {
+    let current = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      const characterIndex = Trie.#getCharacterIndex(word, i);
+      if (!current.children[characterIndex]) {
+        return false;
+      }
+
+      current = current.children[characterIndex]!;
+    }
+
+    return true;
+  }
+
+  hasWord(word: string) {
+    let current = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      const characterIndex = Trie.#getCharacterIndex(word, i);
+      if (!current.children[characterIndex]) {
+        return false;
+      }
+
+      current = current.children[characterIndex]!;
+    }
+
+    return current.wordCount > 0;
+  }
+
+  delete(word: string) {
+    let current = this.root;
+    let lastBranchNode = null;
+
+    for (let i = 0; i < word.length; i++) {
+      const characterIndex = Trie.#getCharacterIndex(word, i);
+      // The word doesn't exist, no delete is required.
+      if (!current.children[characterIndex]) {
+        return false;
+      }
+
+      let count = 0;
+      for (let j = 0; j < 26; j++) {
+        if (current.children[j]) {
+          count++;
+        }
+      }
+
+      if (count > 1) {
+        lastBranchNode = current;
+      }
+
+      current = current.children[characterIndex]!;
+    }
+
+    if (!Trie.#isLeafNode(current)) {
+      current.wordCount--;
+      return true;
+    }
+
+    if (lastBranchNode !== null) {
+      const characterIndex = Trie.#getCharacterIndex(word, word.length - 1);
+      lastBranchNode.children[characterIndex] = null;
+      return true;
+    }
+
+    const characterIndex = Trie.#getCharacterIndex(word, 0);
+    this.root.children[characterIndex] = null;
+    return true;
+  }
+
+  static #getCharacterIndex(word: string, characterPosition: number) {
+    return parseInt(word.charAt(characterPosition), 36) - 10;
+  }
+
+  static #isLeafNode(node: TrieNode) {
+    for (let i = 0; i < node.children.length; i++) {
+      if (node.children[i]) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
